9abf7c8ae97d23122608c1694ace99b54a2f7664
Student <Student@DESKTOP-EF9PEQ2>
2019-09-03 15:54:08 +0900

    로그인 성공시 이전 페이지로 이동시켜주기

--- myPortfolio/src/main/java/kr/ac/kopo/controller/RootController.java
+++ myPortfolio/src/main/java/kr/ac/kopo/controller/RootController.java
@@ -1,5 +1,6 @@
 package kr.ac.kopo.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -15,15 +16,20 @@ public class RootController {
 	}
 
 	@RequestMapping(value="/login", method=RequestMethod.GET)
-	String login() {
+	String login(HttpServletRequest req) {
+		
+		String referer = req.getHeader("Referer");//이전 페이지에 대한 정보가 들어있는 헤더
+		req.getSession().setAttribute("redirectURI", referer);//가져온 uri를 세션에 저장
 		
 		return "login";
 	}
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
 	String login(String id, String pw, HttpSession session) {
+		String backUri = (String)session.getAttribute("redirectURI");//get에서 저장한 uri를 가져다 사용한다.
 		if(id.equals("user") && pw.equals("1234")) {
 			session.setAttribute("user", id);
+			return "redirect:"+backUri;// 로그인 성공시 이전 페이지로 이동시켜준다.
 		}
 		return "redirect:/";
 	}
